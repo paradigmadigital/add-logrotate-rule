447ae93142cac910fc2610e2a8cdfa90b4801b55
jamatute <jamatute@paradigmadigital.com>
2017-09-07 13:30:45 +0200

    * [fix] correct test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_logrotate_file(host):
+    f = host.file('/etc/logrotate.d/my_awesome_program.conf')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.contains('daily')
